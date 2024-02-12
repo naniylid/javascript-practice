c54aa0bebbe0b624f93a17adcb602889ee0b2959
Nadezhda Ilina <nadiailina9@gmail.com>
2024-02-12 13:09:15 +0300

    hoverboard

--- script.js
+++ script.js
@@ -1,63 +1,29 @@
-const addBtn = document.getElementById('add');
+const container = document.getElementById('container');
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
+const SQUARES = 500;
 
-const notes = JSON.parse(localStorage.getItem('notes'));
+for (let i = 0; i < SQUARES; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
 
-if (notes) {
-  notes.forEach((note) => addNewNote(note));
+  square.addEventListener('mouseover', () => setColor(square));
+
+  square.addEventListener('mouseout', () => removeColor(square));
+
+  container.appendChild(square);
 }
 
-addBtn.addEventListener('click', () => addNewNote());
-
-function addNewNote(text = '') {
-  const note = document.createElement('div');
-  note.classList.add('note');
-
-  note.innerHTML = `
-    <div class="tools">
-        <button class="edit"><i class="fas fa-edit"></i></button>
-        <button class="delete"><i class="fas fa-trash-alt"></i></button>
-    </div>
-
-    <div class="main ${text ? '' : 'hidden'}"></div>
-    <textarea class="${text ? 'hidden' : ''}"></textarea>
-    `;
-
-  const editBtn = note.querySelector('.edit');
-  const deleteBtn = note.querySelector('.delete');
-  const main = note.querySelector('.main');
-  const textArea = note.querySelector('textarea');
-
-  textArea.value = text;
-  main.innerHTML = marked(text);
-
-  deleteBtn.addEventListener('click', () => {
-    note.remove();
-
-    updateLS();
-  });
-
-  editBtn.addEventListener('click', () => {
-    main.classList.toggle('hidden');
-    textArea.classList.toggle('hidden');
-  });
-
-  textArea.addEventListener('input', (e) => {
-    const { value } = e.target;
-
-    main.innerHTML = marked(value);
-
-    updateLS();
-  });
-
-  document.body.appendChild(note);
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.background = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 }
 
-function updateLS() {
-  const notesText = document.querySelectorAll('textarea');
+function removeColor(element) {
+  element.style.background = '#1d1d1d';
+  element.style.boxShadow = '0 0 2px #000';
+}
 
-  const notes = [];
-
-  notesText.forEach((note) => notes.push(note.value));
-
-  localStorage.setItem('notes', JSON.stringify(notes));
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
 }
