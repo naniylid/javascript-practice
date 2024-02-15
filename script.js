075f10f4adaffb5ca725029cc630b9a0a7b4e12f
Nadezhda Ilina <nadiailina9@gmail.com>
2024-02-15 11:52:28 +0300

    form submit

--- script.js
+++ script.js
@@ -1,29 +1,9 @@
-const container = document.getElementById('container');
-const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
-const SQUARES = 500;
+const password = document.getElementById('password');
+const background = document.getElementById('background');
 
-for (let i = 0; i < SQUARES; i++) {
-  const square = document.createElement('div');
-  square.classList.add('square');
-
-  square.addEventListener('mouseover', () => setColor(square));
-
-  square.addEventListener('mouseout', () => removeColor(square));
-
-  container.appendChild(square);
-}
-
-function setColor(element) {
-  const color = getRandomColor();
-  element.style.background = color;
-  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
-}
-
-function removeColor(element) {
-  element.style.background = '#1d1d1d';
-  element.style.boxShadow = '0 0 2px #000';
-}
-
-function getRandomColor() {
-  return colors[Math.floor(Math.random() * colors.length)];
-}
+password.addEventListener('input', (e) => {
+  const input = e.target.value;
+  const length = input.length;
+  const blurValue = 20 - length * 2;
+  background.style.filter = `blur(${blurValue}px)`;
+});
