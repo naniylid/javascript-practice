f97994a88baa848f08cd7eaa84f6b541eee273f4
Nadezhda Ilina <nadiailina9@gmail.com>
2024-02-18 08:12:05 +0300

    todos

--- script.js
+++ script.js
@@ -1,98 +1,65 @@
-const quizData = [
-  {
-    question: 'Which language runs in a web browser?',
-    a: 'Java',
-    b: 'C',
-    c: 'Python',
-    d: 'JavaScript',
-    correct: 'd',
-  },
-  {
-    question: 'What does CSS stand for?',
-    a: 'Central Style Sheets',
-    b: 'Cascading Style Sheets',
-    c: 'Cascading Simple Sheets',
-    d: 'Cars SUVs Sailboats',
-    correct: 'b',
-  },
-  {
-    question: 'What does HTML stand for?',
-    a: 'Hypertext Markup Language',
-    b: 'Hypertext Markdown Language',
-    c: 'Hyperloop Machine Language',
-    d: 'Helicopters Terminals Motorboats Lamborginis',
-    correct: 'a',
-  },
-  {
-    question: 'What year was JavaScript launched?',
-    a: '1996',
-    b: '1995',
-    c: '1994',
-    d: 'none of the above',
-    correct: 'b',
-  },
-];
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const todosUL = document.getElementById('todos');
 
-const quiz = document.getElementById('quiz');
-const answerEls = document.querySelectorAll('.answer');
-const questionEl = document.getElementById('question');
-const a_text = document.getElementById('a_text');
-const b_text = document.getElementById('b_text');
-const c_text = document.getElementById('c_text');
-const d_text = document.getElementById('d_text');
-const submitBtn = document.getElementById('submit');
+const todos = JSON.parse(localStorage.getItem('todos'));
 
-let currentQuiz = 0;
-let score = 0;
-
-loadQuiz();
-
-function loadQuiz() {
-  deselectAnswers();
-
-  const currentQuizData = quizData[currentQuiz];
-
-  questionEl.innerText = currentQuizData.question;
-  a_text.innerText = currentQuizData.a;
-  b_text.innerText = currentQuizData.b;
-  c_text.innerText = currentQuizData.c;
-  d_text.innerText = currentQuizData.d;
+if (todos) {
+  todos.forEach((todo) => addTodo(todo));
 }
 
-function deselectAnswers() {
-  answerEls.forEach((answerEl) => (answerEl.checked = false));
-}
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-function getSelected() {
-  let answer;
+  addTodo();
+});
 
-  answerEls.forEach((answerEl) => {
-    if (answerEl.checked) {
-      answer = answerEl.id;
+function addTodo(todo) {
+  let todoText = input.value;
+
+  if (todo) {
+    todoText = todo.text;
+  }
+
+  if (todoText) {
+    const todoEl = document.createElement('li');
+    if (todo && todo.completed) {
+      todoEl.classList.add('completed');
     }
+
+    todoEl.innerText = todoText;
+
+    todoEl.addEventListener('click', () => {
+      todoEl.classList.toggle('completed');
+      updateLS();
+    });
+
+    todoEl.addEventListener('contextmenu', (e) => {
+      e.preventDefault();
+
+      todoEl.remove();
+      updateLS();
+    });
+
+    todosUL.appendChild(todoEl);
+
+    input.value = '';
+
+    updateLS();
+  }
+}
+
+function updateLS() {
+  todosEl = document.querySelectorAll('li');
+
+  const todos = [];
+
+  todosEl.forEach((todoEl) => {
+    todos.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains('completed'),
+    });
   });
 
-  return answer;
+  localStorage.setItem('todos', JSON.stringify(todos));
 }
-
-submitBtn.addEventListener('click', () => {
-  const answer = getSelected();
-
-  if (answer) {
-    if (answer === quizData[currentQuiz].correct) {
-      score++;
-    }
-
-    currentQuiz++;
-
-    if (currentQuiz < quizData.length) {
-      loadQuiz();
-    } else {
-      quiz.innerHTML = `
-              <h2>You answered ${score}/${quizData.length} questions correctly</h2>
-
-              <button onclick="location.reload()">Reload</button>
-          `;
-    }
-  }
-});
